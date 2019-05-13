c4c5e5a1c4875ca8bd917086fb0928ba30303cc9
Rodrigo Shinoda <32286127+rodrigordgfs@users.noreply.github.com>
2019-05-13 11:35:39 -0300

    Challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+    var x = ['teste', 1, true, 1.5, 'Hello World'];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+    function showArray(arg) {
+        return arg;
+    }
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+    showArray(x)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,22 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function showParams(arg, i) {
+    return arg[i];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var z = [1, 4.3, true, "Hello World", 44];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+function showNewArray(arg) {
+    return z;
+}
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +53,51 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+    function book(bookName) {
+        var allBooks = {
+            'JavaScript Book': {
+                quantidadePaginas: 254,
+                autor: 'Dev1',
+                editora: 'Hello World'
+            },
+            'Vue.JS Book': {
+                quantidadePaginas: 364,
+                autor: 'Dev2',
+                editora: 'Hello World'
+            },
+            'HTML5 Book': {
+                quantidadePaginas: 145,
+                autor: 'Dev3',
+                editora: 'Hello World'
+            }
+        };
+        
+        return !bookName ? allBooks : allBooks[bookName];
+        
+    }
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+    console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+    console.log("O livro Vue.JS tem " + book('Vue.JS Book').quantidadePaginas + " páginas!");
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log("O autor do livro livro Vue.JS é " + book('Vue.JS Book').autor);
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log("O livro livro Vue.JS foi publicado pela editora " + book('Vue.JS Book').editora);
